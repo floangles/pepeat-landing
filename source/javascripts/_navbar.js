5fc5d08411ab6926333b88770c8a801f3ba6646a
florent angles <florent.angless@gmail.com>
2015-07-16 11:26:12 +0200

    first commit

---
+++ source/javascripts/_navbar.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+  // navbar transition jQuery script
+  $(window).scroll(function(e){
+    if ($(this).scrollTop() > 400) {
+      $(".navbar").css({
+        "background": "rgba(2,2,2,0.85)",
+        "box-shadow": "0 0 2px black"
+      });
+    }
+    else {
+      $(".navbar").css({
+        "background": "transparent",
+        "box-shadow": "0 0 0px transparent"
+      });
+    }
+  });
+  if ($(this).scrollTop() > 400) {
+      $(".navbar").css({
+        "background": "rgba(2,2,2,0.8)",
+        "box-shadow": "0 0 2px black"
+      });
+    }
+    else {
+      $(".navbar").css({
+        "background": "transparent",
+        "box-shadow": "0 0 0px transparent"
+      });
+    }
+});
+
+
+
