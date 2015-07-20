3a0f04ed30a4b2a02630f6f9c5d7551c6520995f
florent angles <florent.angless@gmail.com>
2015-07-20 13:58:52 +0200

    onligne push

--- source/javascripts/_social_media.js
+++ source/javascripts/_social_media.js
@@ -5,3 +5,23 @@
   js.src = "//connect.facebook.net/fr_FR/sdk.js#xfbml=1&version=v2.4";
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
+
+
+
+
+window.twttr = (function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0],
+    t = window.twttr || {};
+  if (d.getElementById(id)) return t;
+  js = d.createElement(s);
+  js.id = id;
+  js.src = "https://platform.twitter.com/widgets.js";
+  fjs.parentNode.insertBefore(js, fjs);
+
+  t._e = [];
+  t.ready = function(f) {
+    t._e.push(f);
+  };
+
+  return t;
+}(document, "script", "twitter-wjs"));
